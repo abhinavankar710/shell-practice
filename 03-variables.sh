0e50ee8448eb366cae636839867b49e5e0124a87
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-26 16:01:38 +0530

    Conversation of John and Raheem

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# No space before or after equal sign while assigning value to variable.
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2,How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am also fine. What are you doing recently?"
+echo "$PERSON2:: I am learning shell scripting. It is very interesting $PERSON1."
