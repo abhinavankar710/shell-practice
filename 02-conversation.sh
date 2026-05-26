9c7817ef72b874882143b3d1044c690e62f4db0d
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-26 15:49:05 +0530

    Conversation of Ramesh and Suresh

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh,How are you?"
+echo "Suresh:: Hello Ramesh, I am fine. How are you?"
+echo "Ramesh:: I am also fine. What are you doing recently?"
+echo "Suresh:: I am learning shell scripting. It is very interesting Ramesh."
