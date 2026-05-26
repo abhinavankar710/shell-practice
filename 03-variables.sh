2892f3ec9c6498dc8c08d4a0d338281c2f09caa1
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-26 16:04:21 +0530

    Conversation of Ronaldo and Virat

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@
 PERSON1=Ronaldo
 PERSON2=Virat
 
-echo "$PERSON1:: Hi $PERSON2,How are you?"
+echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am also fine. What are you doing recently?"
 echo "$PERSON2:: I am learning shell scripting. It is very interesting $PERSON1."
