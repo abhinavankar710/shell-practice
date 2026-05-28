c43c19f0d193364c9b7381e8a10ef73a138c4cb2
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-28 11:15:13 +0530

    Data Types

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -8,3 +8,10 @@ NAME=DevOps
 SUM=$(($NUMBER1 + $NUMBER2 + $NAME)) 
 
 echo "SUM is: ${SUM}"
+
+LEADERS="Mahatma Gandhi, Martin Luther King Jr., Nelson Mandela"
+
+echo "All Famous Leaders are: ${LEADERS[@]}"
+echo "First Leader is: ${LEADERS[0]}"
+echo "Second Leader is: ${LEADERS[1]}"
+echo "Third Leader is: ${LEADERS[2]}"
