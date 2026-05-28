cde1404cc5a3e6278b8d3ccd5c1a148aeb182bc5
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-28 12:18:03 +0530

    conditions

---
+++ 12-installation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USER= echo "$USER"
+
+if [$USER == "root"]; then
+    
+    dnf install mysql -y && if [ $? -eq 0 ]; then
+        echo "MySQL installed successfully"
+    else
+        echo "MySQL installation failed"
+    fi
+
+else
+    echo "You are logged in as $USER user please take root user access to install MySQL"
+fi
